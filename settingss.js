83e3cc46e5c934f68b2fae741568c6fe2cce28ea
Sami-PipsTechnology <saminno256@gmail.com>
2025-09-07 18:25:08 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~tqNT0KzR#kj0jqIIt3Ia1Z2pxGkxIhHaf-BzBENZk0oaCF8mAT0I",  // add your Session Id here
+  SESSION_ID: "malvin~FrcUDRKY#q4G8uRw1_bDlL0L-gItJv_Y0VEygDVqZ_NMD026FcoU",  // add your Session Id here
   
   OWNER_NUMBER: "256766104887", // put your phone number here
   
